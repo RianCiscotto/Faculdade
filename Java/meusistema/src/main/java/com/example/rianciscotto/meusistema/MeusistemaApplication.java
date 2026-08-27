d42324f19b527a1851ed0e3b657cabc05153b8a9
Rian Ciscotto <121577739+RianCiscotto@users.noreply.github.com>
2026-08-27 19:43:20 -0300

    Add files via upload

---
+++ Java/meusistema/src/main/java/com/example/rianciscotto/meusistema/MeusistemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rianciscotto.meusistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusistemaApplication.class, args);
+	}
+
+}
---
+++ Java/meusistema/src/test/java/com/example/rianciscotto/meusistema/MeusistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rianciscotto.meusistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
